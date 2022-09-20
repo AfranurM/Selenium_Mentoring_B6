ad4d8182af87301f607a4da210f6eb1908a2ec66
AfranurM <afranurmenemenli1103@gmail.com>
2022-09-20 18:48:56 -0400

    day17

---
+++ src/Day17/_2_TwoString.java
@@ -0,0 +1,30 @@
+package Day17;
+
+import java.util.Scanner;
+
+public class _2_TwoString {
+
+    /*
+            Get two Strings by using scanner class
+            add the second string after the first one
+            If the first word ends with the same letter as the second word starts with, then remove it.
+            Example: "abc", "cat" --> "abcat"
+                   "abc", "dog"  -->"abcdog"
+        */
+
+    public static void main(String[] args) {
+
+        System.out.println("please enter two words: ");
+        Scanner scanner = new Scanner(System.in);
+        String userString = scanner.nextLine();
+        String[] words = userString.split(" ");
+        String word1 = words[0];
+        String word2 = words[1];
+
+        if(word1.charAt(word1.length()-1) == word2.charAt(0)){
+            System.out.println(word1.concat(word2.substring(1)));
+        }else{
+            System.out.println(word1.concat(word2));
+        }
+    }
+}
