99b4815f241d7580a173f3a6fb71b57bebf88dc9
AfranurM <afranurmenemenli1103@gmail.com>
2022-09-26 18:50:47 -0400

    day18

--- src/Day18/_1_StringMethods.java
+++ src/Day18/_1_StringMethods.java
@@ -4,14 +4,6 @@ import java.util.Arrays;
 
 public class _1_StringMethods {
 
-    public static void main(String[] args) {
+    // I will be adding the examples here
 
-
-        String str = "Techno Study";
-        String str2 = "Batch Six";
-
-        String [] arr = str.split(str2);
-
-        System.out.println(Arrays.toString(arr));
-    }
 }
