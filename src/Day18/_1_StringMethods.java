1fa377f2f3bd81d6a8143d681a65f29d27519929
AfranurM <afranurmenemenli1103@gmail.com>
2022-09-27 18:04:57 -0400

    day18

--- src/Day18/_1_StringMethods.java
+++ src/Day18/_1_StringMethods.java
@@ -4,6 +4,30 @@ import java.util.Arrays;
 
 public class _1_StringMethods {
 
-    // I will be adding the examples here
+    public static void main(String[] args) {
+        /*
 
+        startsWith ---- return type is boolean
+        endsWith ----- return type is boolean
+        toLowerCase ---- return type is String
+        ToUpperCase ---- return type is String
+        Length ---- return type is int
+        indexOf ---- return type is int
+        lastIndexOf ---- return type is int
+        charAt ---- return type is char
+        isEmpty ---- return type is boolean
+        contains ---- return type is boolean
+        concat ---- return type is string
+        substring ---- return type is string
+        replace ---- return type is string
+        replaceAll ---- return type is string
+        trim ---- return type is string
+        equalsAndEqualsIgnoreCase ---- return type is boolean
+        toString ---- return type is string
+        split -- ---- return type is int String array
+
+        NOTE: Examples are on day 19
+
+         */
+    }
 }
