fab23fc160fe22fec69bff54bbf95c082cfb8b54
AfranurM <afranurmenemenli1103@gmail.com>
2022-09-28 16:27:03 -0400

    day19

--- src/day19/_2_StringMethodsTask.java
+++ src/day19/_2_StringMethodsTask.java
@@ -1,5 +1,8 @@
 package day19;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class _2_StringMethodsTask {
 
     // Use scanner class and get the first name from user: firstName
@@ -9,6 +12,18 @@ public class _2_StringMethodsTask {
 
     public static void main(String[] args) {
 
-        String message = " If your phone number is 111-222-3333, please confirm this message! ";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter yur first name: ");
+        String firstName = scanner.nextLine();
+
+        String sentence = " If your phone number is 111-222-3333, please confirm this message! ";
+        String substringThePart = sentence.substring(4,37);
+        System.out.println(substringThePart);
+        String [] splitFromFirstDash = substringThePart.split("-", 2);
+        System.out.println(Arrays.toString(splitFromFirstDash));
+        System.out.println(splitFromFirstDash[0]);
+        String theLastPartReplacedWithStar = splitFromFirstDash[1].replaceAll("[0-9]", "*");
+        System.out.println(theLastPartReplacedWithStar);
+        System.out.println(firstName + ", " + splitFromFirstDash[0].concat(theLastPartReplacedWithStar.replaceAll("-","")));
     }
 }
